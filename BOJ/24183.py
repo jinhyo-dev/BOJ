59fff6c28b70e60d6eea6180d2e188bc7dc6d09f
jinhyo-dev <jinhyo0850@gmail.com>
2022-02-16 23:43:20 +0900

    2/16 23:43

---
+++ BOJ/24183.py
@@ -0,0 +1,6 @@
+A , B, C = 0.074196, 0.124740, 0.062370
+X, Y, Z = map(int, input().split())
+A *= X
+B *= Y
+C *= Z
+print((A*2) + (B*2) + C)
---
+++ BOJ/24309.py
@@ -0,0 +1,5 @@
+A = int(input())
+B = int(input())
+C = int(input())
+X = (B - C) // A
+print(X)
