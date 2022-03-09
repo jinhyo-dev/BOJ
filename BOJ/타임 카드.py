2fbccb7a1e9133e17d60b3e853c1768ddb49d7a1
jinhyo-dev <jinhyo0850@gmail.com>
2022-03-09 14:59:35 +0900

    3/9 14:59

---
+++ BOJ/타임 카드.py
@@ -0,0 +1,9 @@
+for _ in range(3):
+  h1, m1,s1, h2, m2, s2 = map(int, input().split())
+  t1 = h1*3600 + m1*60 + s1
+  t2 = h2*3600 + m2*60 + s2
+  t = t2 - t1
+  h = t // 3600 % 24
+  m = t // 60 % 60
+  s = t % 60
+  print(h, m, s)
