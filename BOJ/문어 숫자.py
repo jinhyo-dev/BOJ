36987a7d5a561b9675647af0ecc3656bab28002f
jinhyo-dev <jinhyo0850@gmail.com>
2022-04-11 11:35:27 +0900

    4/11 11:35

---
+++ BOJ/문어 숫자.py
@@ -0,0 +1,22 @@
+from re import S
+
+
+while True:
+  sum = 0
+  oct = list(input())
+  squared = len(oct)-1
+  if oct[0] == '#':
+    break
+  else:
+    for i in oct:
+      if i == '-':  sum += (0 * (8 ** squared))
+      elif i == "\\": sum += (1 * (8 ** squared))
+      elif i == "(": sum += (2 * (8 ** squared))
+      elif i == "@": sum += (3 * (8 ** squared))
+      elif i == "?": sum += (4 * (8 ** squared))
+      elif i == ">": sum += (5 * (8 ** squared))
+      elif i == "&": sum += (6 * (8 ** squared))
+      elif i == "%": sum += (7 * (8 ** squared))
+      else: sum += (-1 * (8 ** squared))
+      squared -= 1
+    print(sum)
