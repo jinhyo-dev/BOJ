7022a102edf448d41f33eef803f2e0190d598ac3
jinhyo-dev <jinhyo0850@gmail.com>
2022-04-15 16:54:07 +0900

    4/15 16:54

---
+++ BOJ/조합 0의 개수.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+five = [5**i for i in range(1, 15)]
+two = [2**i for i in range(1, 32)]
+
+def sollution(N, arr):
+  result = 0
+  for i in arr:
+    if N // i == 0:
+      return result
+    result += N // i
+
+print(min(sollution(N, five) - (sollution(N-M, five) + sollution(M, five)), sollution(N, two) - (sollution(N-M, two) - sollution(M, two))))
