d9f2ce4fb0b4fa9d639828076b821117505c130e
jinhyo-dev <jinhyo0850@gmail.com>
2022-06-13 17:53:04 +0900

    6/13 17:53

---
+++ 2×n 타일링 2.py
@@ -0,0 +1,4 @@
+arr = [0, 1, 3]
+for i in range(3, 1001):
+  arr.append((arr[i-2] * 2) + arr[i-1])
+print(arr[int(input())] % 10007)
--- 팩토리얼.py
+++ 팩토리얼.py
@@ -1,3 +1,2 @@
-def factorial(n):
-  return 1 if n <= 1 else n * factorial(n-1)
+from math import factorial
 print(factorial(int(input())))
